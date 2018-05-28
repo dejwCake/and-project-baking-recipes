8f2c3d98843b04729b951e916c1b966b003f967c
David Běhal <behal.david@gmail.com>
2018-05-28 23:56:01 +0200

    Prepared player

--- app/src/main/java/sk/dejw/android/bakingrecipes/RecipeStepFragment.java
+++ app/src/main/java/sk/dejw/android/bakingrecipes/RecipeStepFragment.java
@@ -1,24 +1,43 @@
 package sk.dejw.android.bakingrecipes;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.media.session.MediaSessionCompat;
+import android.support.v4.media.session.PlaybackStateCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.PlaybackParameters;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
+import com.google.android.exoplayer2.source.TrackGroupArray;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
+import com.google.android.exoplayer2.ui.PlayerView;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import sk.dejw.android.bakingrecipes.models.RecipeStep;
 
-public class RecipeStepFragment extends Fragment {
+public class RecipeStepFragment extends Fragment implements ExoPlayer.EventListener {
 
     public static final String RECIPE_STEP = "recipe_step";
+    private static final String TAG = RecipeStepFragment.class.getSimpleName();
 
     private RecipeStep mRecipeStep;
 
+    @BindView(R.id.player_view)
+    PlayerView mPLayerView;
     @BindView(R.id.tv_step_description)
     TextView mDescription;
+
+    private SimpleExoPlayer mExoPlayer;
+    private static MediaSessionCompat mMediaSession;
+    private PlaybackStateCompat.Builder mStateBuilder;
 
     public RecipeStepFragment() {
     }
@@ -36,11 +55,52 @@ public class RecipeStepFragment extends Fragment {
         mDescription.setText(mRecipeStep.getDescription());
 
         //TODO setup layout
+
+        if(mRecipeStep.getVideoURL() != null) {
+            initializeMediaSession();
+
+            initializePlayer(Uri.parse(mRecipeStep.getVideoURL()));
+        } else {
+            mPLayerView.setVisibility(View.GONE);
+        }
+
         return rootView;
     }
 
     public void setRecipeStep(RecipeStep recipeStep) {
         mRecipeStep = recipeStep;
+    }
+
+    private void initializeMediaSession() {
+
+        // Create a MediaSessionCompat.
+        mMediaSession = new MediaSessionCompat(this, TAG);
+
+        // Enable callbacks from MediaButtons and TransportControls.
+        mMediaSession.setFlags(
+                MediaSessionCompat.FLAG_HANDLES_MEDIA_BUTTONS |
+                        MediaSessionCompat.FLAG_HANDLES_TRANSPORT_CONTROLS);
+
+        // Do not let MediaButtons restart the player when the app is not visible.
+        mMediaSession.setMediaButtonReceiver(null);
+
+        // Set an initial PlaybackState with ACTION_PLAY, so media buttons can start the player.
+        mStateBuilder = new PlaybackStateCompat.Builder()
+                .setActions(
+                        PlaybackStateCompat.ACTION_PLAY |
+                                PlaybackStateCompat.ACTION_PAUSE |
+                                PlaybackStateCompat.ACTION_SKIP_TO_PREVIOUS |
+                                PlaybackStateCompat.ACTION_PLAY_PAUSE);
+
+        mMediaSession.setPlaybackState(mStateBuilder.build());
+
+
+        // MySessionCallback has methods that handle callbacks from a media controller.
+        mMediaSession.setCallback(new MySessionCallback());
+
+        // Start the Media Session since the activity is active.
+        mMediaSession.setActive(true);
+
     }
 
     /**
@@ -50,5 +110,65 @@ public class RecipeStepFragment extends Fragment {
     public void onSaveInstanceState(Bundle currentState) {
         currentState.putParcelable(RECIPE_STEP, mRecipeStep);
         super.onSaveInstanceState(currentState);
+    }
+
+    /**
+     * Release the player when the activity is destroyed.
+     */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        releasePlayer();
+        mMediaSession.setActive(false);
+    }
+
+    @Override
+    public void onTimelineChanged(Timeline timeline, Object manifest, int reason) {
+
+    }
+
+    @Override
+    public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
+
+    }
+
+    @Override
+    public void onLoadingChanged(boolean isLoading) {
+
+    }
+
+    @Override
+    public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+
+    }
+
+    @Override
+    public void onRepeatModeChanged(int repeatMode) {
+
+    }
+
+    @Override
+    public void onShuffleModeEnabledChanged(boolean shuffleModeEnabled) {
+
+    }
+
+    @Override
+    public void onPlayerError(ExoPlaybackException error) {
+
+    }
+
+    @Override
+    public void onPositionDiscontinuity(int reason) {
+
+    }
+
+    @Override
+    public void onPlaybackParametersChanged(PlaybackParameters playbackParameters) {
+
+    }
+
+    @Override
+    public void onSeekProcessed() {
+
     }
 }
